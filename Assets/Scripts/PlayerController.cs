b467d624f0c729a1ba4acf875772533cd41d3221
Kaj <zshadowcast@gmail.com>
2016-05-12 23:52:20 -0500

    Added Gyroscope controls, adjusted the speed and made the win text bigger

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-	
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 
 		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+
+		if (count == 8) {
+			winText.text = "You Win!";
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
 	}
 }
